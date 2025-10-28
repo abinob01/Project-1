193343f7df198a4da86d1aa6e6768f3ee3e78af9
abinob01 <abinob70@gmail.com>
2025-10-28 10:41:32 -0700

    Duplicate files Remove(code optimization)

--- script.js
+++ script.js
@@ -2,31 +2,28 @@
 
 const hiddenNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+const newBB = function (message) {
+  document.querySelector('.M1').textContent = message;
+};
 document.querySelector('.btn2').addEventListener('click', function () {
   const inputGuess = Number(document.querySelector('input').value);
   if (inputGuess === hiddenNumber) {
-    document.querySelector('.M1').textContent = '🙀 Correct Guess!';
+    newBB('🙀 Correct Guess!');
     if (score > Number(document.querySelector('.M3').textContent)) {
       document.querySelector('.M3').textContent = score;
     }
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.dd').textContent = hiddenNumber;
     document.querySelector('.dd').style.width = '160px';
-  } else if (inputGuess > hiddenNumber) {
-    if (score >= 1) {
-      document.querySelector('.M1').textContent = '📈 Too High!';
+  } else if (inputGuess !== hiddenNumber) {
+    if (score > 1) {
+      newBB(inputGuess > hiddenNumber ? '📈 Too High!' : '📉 Too Low!');
       score--;
       document.querySelector('.M2').textContent = score;
     } else {
-      document.querySelector('.M1').textContent = '💥 You Lost the Game!';
-    }
-  } else if (inputGuess < hiddenNumber) {
-    if (score >= 1) {
-      document.querySelector('.M1').textContent = '📉 Too Low!';
-      score--;
-      document.querySelector('.M2').textContent = score;
-    } else {
-      document.querySelector('.M1').textContent = '💥 You Lost the Game!';
+      newBB('💥 You Lost the Game!');
+      document.querySelector('.M2').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
     }
   }
 });
@@ -34,7 +31,7 @@ document.querySelector('.btn2').addEventListener('click', function () {
 document.querySelector('.btn1').addEventListener('click', function () {
   document.querySelector('.M2').textContent = 20;
   score = 20;
-  document.querySelector('.M1').textContent = 'Start guessing...';
+  newBB('Start guessing...');
   document.querySelector('input').value = '';
   document.querySelector('body').style.backgroundColor = 'bisque';
   document.querySelector('.dd').textContent = '?';
